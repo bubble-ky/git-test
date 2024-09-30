c4926fc4f7c65068a0e43024e84cdaf12b136a05
bubble-ky <120250920@qq.com>
2024-09-30 13:36:56 +0800

    dev1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git 2");
         System.out.println("hello gig 3");
+        System.out.println("dev m1");
     }
 }
